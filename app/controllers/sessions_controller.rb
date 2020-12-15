77ddb8c63b59b97ab8663da5b52c0b84307d24de
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-12-15 20:44:38 +0100

    Sessions work.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+
+    if @user&.authenticate(params[:session][:password])
+      log_in(@user)
+      redirect_to @user
+    else
+      redirect_to login_path, alert: 'Invalid email/password combination'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to login_path
+  end
+end
