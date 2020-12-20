f410b95947bdcb77312163bc151b3db09aba2380
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-12-20 18:54:18 +0100

    Some smartlink functionality.

--- app/controllers/smartlinks_controller.rb
+++ app/controllers/smartlinks_controller.rb
@@ -1,13 +1,16 @@
 class SmartlinksController < ApplicationController
   before_action :valid_slug, only: :redirect
+  before_action :logged_in, except: :redirect
 
   def redirect
     if (smartlink = Smartlink.find_by(slug: params[:slug]))
       redirections = smartlink.redirections.joins(:language)
-      if (url = redirections.find_by('languages.code = ?', request.accept_language[0..1]).url)
+      if (url = redirections.find_by('languages.code = ?', request.accept_language[0..1])&.url)
         redirect_to url
+      elsif (default_url = redirections.find_by("languages.default = 'true'")&.url)
+        redirect_to default_url
       else
-        redirect_to redirections.find_by("languages.default = 'true'").url
+        redirect_to home_path, alert: 'Neither a smartlink with your nrowser language nor default one for this slug is found'
       end
     else
       redirect_to home_path, alert: 'There is no smartlink with this slug'
@@ -15,7 +18,7 @@ class SmartlinksController < ApplicationController
   end
 
   def index
-    current_user.smartlinks
+    current_user.smartlinks if logged_in?
   end
 
   def new
@@ -23,7 +26,9 @@ class SmartlinksController < ApplicationController
   end
 
   def create
-    @smartlink = Smartlink.new(smartlink_params)
+    @smartlink = current_user.smartlinks.new(slug: smartlink_params[:slug])
+    language = Language.find_or_initialize_by(code: smartlink_params[:language_code])
+    @smartlink.redirections.new(smartlink: @smartlink, language: language, url: smartlink_params[:url])
 
     respond_to do |format|
       if @smartlink.save
@@ -44,10 +49,14 @@ class SmartlinksController < ApplicationController
   private
 
   def valid_slug
-    redirect_to home_path, alert: 'Invalid slug' unless params[:slug].match(VALID_SLUG)
+    redirect_to root_path, alert: 'Invalid slug' unless params[:slug].match(VALID_SLUG)
   end
 
   def smartlink_params
-    params.require(:smartlink).permit(:slug, :language, :url)
+    params.require(:smartlink).permit(:slug, :language_code, :url)
+  end
+
+  def logged_in
+    redirect_to login_path, alert: 'Please login first' unless logged_in?
   end
 end
