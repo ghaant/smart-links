b4c2382999b254e2041a29d6faa73c5c5379c935
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-12-20 18:52:10 +0100

    English language is et default automatically.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,11 @@
 class Language < ApplicationRecord
+  before_save :set_default
+
   has_many :redirections, dependent: :destroy
+
+  private
+
+  def set_default
+    self.default = true if code == 'en'
+  end
 end
