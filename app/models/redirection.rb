59aa59e8d5185ac44a2af536102897caf986338e
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-12-21 17:07:17 +0100

    Smartlink model specs.

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,6 +1,5 @@
 class Redirection < ApplicationRecord
-  validates :url, presence: true,
-                  length: { minimum: 12 }
+  validates :url, presence: true, length: { minimum: 12 }
 
   belongs_to :smartlink
   belongs_to :language
